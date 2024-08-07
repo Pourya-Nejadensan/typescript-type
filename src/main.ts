080c323098c57e18c28c72781db4a4dd34aacd51
Pourya <pourya.nejadensan@gmail.com>
2024-08-07 11:48:00 +0200

    Step 2: Allow strings and numbers as grades

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ const numbers: number[] = [1,4,3,1,3,2,1,2]
 const studen: Student = {
     firstName: 'Anton',
     lastName: 'Meier',
-    age: 0,
+    age: 17,
     grades: numbers
 }
 
@@ -31,4 +31,44 @@ console.log(
     studen.firstName + ' ' + studen.lastName + ' (' + studen.age + ')', '\n',
     '==============================', '\n',
     'Grades: ', studen.grades.toString()
+)
+
+/*
+Step 2: Allow strings and numbers as grades
+Task:
+
+
+    Ensure that besides grades as numbers (German system), values A to F (American system) are also supported as grades. The function should output the student's grades.
+
+
+Example:
+
+
+    Anton Meier (17)
+==============================
+
+Noten:
+A,2,F,3,1,B,2,5
+*/
+
+type Grade = number | string;
+
+type StudentUpdate2 = {
+    firstName: string,
+    lastName: string,
+    age: number,
+    grades: Grade[]
+}
+
+const studentUpdate2: StudentUpdate2 = {
+    firstName: 'Anton',
+    lastName: 'Meier',
+    age: 17,
+    grades: ['A', 2, 'F', 3, 1, 'B', 2, 5]
+}
+
+console.log(
+    studentUpdate2.firstName + ' ' + studentUpdate2.lastName + ' (' + studentUpdate2.age + ')', '\n',
+    '==============================', '\n',
+    'Noten: ', studentUpdate2.grades.toString()
 )
